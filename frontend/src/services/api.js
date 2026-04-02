c6e6e311f08bc2e4cd704b07f6ccb4a877a3fb1b
Ann Karagwa <annkaragwa@gmail.com>
2026-04-02 19:40:07 +0300

    Refactor: Update API base URL configuration to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : "http://127.0.0.1:8000/api";
 
 // Create axios instance
 const api = axios.create({
